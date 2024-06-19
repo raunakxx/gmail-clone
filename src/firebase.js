b98eb4cc621f38a3bb0f7821b70acbc8d6870142
prabhat kumar raunak <63907515+raunakxx@users.noreply.github.com>
2024-06-19 18:11:31 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPEfno16S807u5Tv_vAd-OT6X9x55kSpA",
-  authDomain: "clone-51310.firebaseapp.com",
-  projectId: "clone-51310",
-  storageBucket: "clone-51310.appspot.com",
-  messagingSenderId: "682002261520",
-  appId: "1:682002261520:web:f75a231c8ef7d106c94c95",
-  measurementId: "G-XX99CNEJG5",
+
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
